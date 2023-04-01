00ccfae00acfac905d324fcb9252ce020f97dcc6
Evgeniy <yaremenko.x@gmail.com>
2023-04-01 23:41:48 +0300

    update json

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+
+import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery')
+const galleryMarkup = createImgGallery(galleryItems);
+
+function createImgGallery (gallery) {
+    return gallery.map(({preview, original, description}) => {
+        return `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                style="list-style-type: none"
+                src='${preview}'
+                alt='${description}'
+                />
+            </a>
+        </li>`
+    }).join('');
+}
+
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+const createdLightModal = new SimpleLightbox('.gallery a', {
+    
+    captionType: 'attr',
+    captionsData: 'alt',
+    captionDelay: 250
+});
+
