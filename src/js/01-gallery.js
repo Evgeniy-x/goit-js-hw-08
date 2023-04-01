4f4673cf42ed4b7381d706537dc5456c091f0b4e
Evgeniy <yaremenko.x@gmail.com>
2023-04-02 00:05:05 +0300

    test

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,9 +1,9 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
-
-console.log(galleryItems);
 
 const galleryContainer = document.querySelector('.gallery')
 const galleryMarkup = createImgGallery(galleryItems);
